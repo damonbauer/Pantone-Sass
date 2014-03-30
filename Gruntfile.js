645047c2b748837b3b4a1cce4b7c3da9cf03b2fc
frederickk <ken.frederick@gmx.de>
2014-03-29 23:53:39 -0700

    added metallic-coated

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function(grunt) {
 			dist: {
 				files: {
      				"css/pantone-coated.css" : "less/pantone-coated.less",
+     				"css/pantone-metallic-coated.css" : "less/pantone-metallic-coated.less",
      				"css/pantone-pastels-neons-coated.css" : "less/pantone-pastels-neons-coated.less",
      				"css/pantone-uncoated.css" : "less/pantone-uncoated.less",
      				"css/style.css" : "less/style.less"
